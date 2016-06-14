d979d64be85ecd534eed75676747fd348fc11c0c
Kevin Tarr <kgt2110@columbia.edu>
2016-06-14 10:16:28 -0400

    Xcode project

---
+++ HTV_VRSandbox/HTV_VRSandbox/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HTV_VRSandbox
+//
+//  Created by Gboinyee Tarr on 6/10/16.
+//  Copyright © 2016 Kevin Tarr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HTV_VRSandbox/HTV_VRSandbox/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HTV_VRSandbox
+//
+//  Created by Gboinyee Tarr on 6/10/16.
+//  Copyright © 2016 Kevin Tarr. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
